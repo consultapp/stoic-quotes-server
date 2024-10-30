3e1a8d949fcdecb84487986d85fd7837a5a06f8c
Dmitry Egorov <info@consultapp.ru>
2024-10-30 16:17:57 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM  node:20.18-alpine3.20
+RUN npm set strict-ssl false
+
+WORKDIR /src
+
+COPY package.json .
+COPY package-lock.json .
+
+RUN npm install
+
+COPY . .
+
+ENV PORT=3000
+ENV NODE_ENV=production
+
+# RUN npm run start
+ENTRYPOINT npm run start
+
+EXPOSE $PORT
+
+
+
+
---
+++ src/server.js
@@ -0,0 +1,41 @@
+import fs from "node:fs";
+import express from "express";
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+let stoicQuotes = null;
+
+function getRandomQuote() {
+  return stoicQuotes[Math.round(stoicQuotes.length * Math.random())];
+}
+
+fs.readFile("./src/quotes.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const { quotes } = JSON.parse(data);
+  if (quotes.length) stoicQuotes = quotes;
+});
+
+app.get("/", (req, res) => {
+  if (!stoicQuotes) return res.status(500).send("No qoutes...");
+
+  res.status(200).type("application/json");
+  console.log("Stoic Quote Server:", { status: true });
+  res.send(JSON.stringify({ status: true }));
+});
+
+app.get("/random", (req, res) => {
+  if (!stoicQuotes) return res.status(500).send("No qoutes...");
+
+  res.status(200).type("application/json");
+
+  res.send(JSON.stringify(getRandomQuote()));
+});
+
+app.listen(port, host, function () {
+  console.log(`Stoic Quote Server listens http://${host}:${port}`);
+});
